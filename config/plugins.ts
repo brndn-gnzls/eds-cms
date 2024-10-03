e18d862554951af3e598c9fe9ee177e2a7fd2b54
wrvthlss <wrgh.sushi333@passmail.net>
2024-10-03 11:31:25 -0400

    Merged GraphQL functionality into Strapi.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+export default () => ({
+    graphql: {
+        enabled: true,
+        config: {
+            endpoint: '/graphql',
+            shadowCRUD: true, // Expose CRUD operations for collections.
+            playgroundAlways: true, // Non-production.
+            depthLimit: 7, // Limit query depth.
+            amountLimit: 100, // Limit returned items.
+        },
+    },
+});
